3c1a638aff0ac658d4a15e4629f0f4178663cceb
Evergreenn <evergreen@protonmail.ch>
2017-02-24 15:11:37 +0100

    web server handle the request and return a tcp hello world response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let listener = TcpListener::bind("127.0.0.1:45678")
+        .unwrap();
+
+    let stream = listener.accept().unwrap().0;
+    read_request(stream);
+}
+
+fn read_request(mut stream: TcpStream){
+
+    let response = "HTTP/1.1 200 OK\n\n<html><body>Hello, World ! ❤️";
+
+    stream.write(response.as_bytes()).unwrap();
 }
